f5cf8b021bb22b3d40448ca749e36805c80e7453
pyto-p <sbstntmprl@gmail.com>
2023-05-30 17:55:28 +0800

    finished version 1.2

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let attack = document.querySelector(".atk");
 let result = document.querySelector(".result");
 let humanSide = document.querySelector(".human-side");
 let cpuSide = document.querySelector(".cpu-side");
+let humanFlag = document.querySelector(".human-flag");
+let cpuFlag = document.querySelector(".cpu-flag");
+let humanScore = document.querySelector(".human-score");
+let cpuScore = document.querySelector(".cpu-score");
+let humanNum = 0;
+let cpuNum = 0;
 
+console.log(humanNum + 1);
 // CPU's Side
 function getCpuAtk() {
     let computerAtk = Math.floor(Math.random() * 3) + 1;
@@ -34,18 +41,42 @@ function getResult() {
     humanSide.src = humanAtk + ".png";
     cpuSide.src = computerAtk + ".png";
 
-    if (computerAtk == "Rock" && humanAtk == "Scissor") {
-        result.textContent = "Computer Wins!";
-    } else if (computerAtk == "Scissor" && humanAtk == "Paper") {
-        result.textContent = "Computer Wins!";
-    } else if (computerAtk == "Paper" && humanAtk == "Rock") {
-        result.textContent = "Computer Wins!";
-    } else if (computerAtk == humanAtk) {
-        result.textContent = "Draw!";
-    } else if (humanAtk == "None") {
-        result.textContent = "Who will win?";
+    overallNum = cpuNum + humanNum;
+
+    if (overallNum < 4 && (cpuNum < 3 && humanNum < 3)) {
+        if (computerAtk == "Rock" && humanAtk == "Scissor") {
+            cpuNum += 1;
+            cpuFlag.textContent = cpuFlag.textContent + "🚩";
+            cpuScore.innerHTML = cpuNum; 
+            console.log(overallNum);
+        } else if (computerAtk == "Scissor" && humanAtk == "Paper") {
+            cpuNum += 1;
+            cpuScore.innerHTML = cpuNum; 
+            cpuFlag.textContent = cpuFlag.textContent + "🚩";
+            console.log(overallNum);
+        } else if (computerAtk == "Paper" && humanAtk == "Rock") {
+            cpuNum += 1;
+            cpuScore.innerHTML = cpuNum; 
+            cpuFlag.textContent = cpuFlag.textContent + "🚩";
+            console.log(overallNum);
+        } else if (humanAtk == "None") {
+            result.textContent = "Who will win?";
+        } else {
+            humanNum += 1;
+            humanFlag.textContent = humanFlag.textContent + "🚩";
+            humanScore.innerHTML = humanNum; 
+            console.log(overallNum);
+        }
     } else {
-        result.textContent = "Human Wins!";
+        if (humanNum >= 3 && humanNum > cpuNum){
+            result.innerHTML = "HUMAN WINS!";
+        } else {
+            result.textContent = "CPU WINS!";
+        }
+        // cpuNum = 0;
+        // humanNum = 0;
+        // cpuFlag.textContent = "";
+        // humanFlag.textContent = "";
     }
 }
 
